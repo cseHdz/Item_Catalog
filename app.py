769226eb11a58969fbd18c58ddc52ec4ef4309dc
Carlos Hernandez <carlos.hdz@me.com>
2018-03-13 23:10:10 -0400

    Removed debug print statements

--- app.py
+++ app.py
@@ -81,7 +81,6 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -119,7 +118,6 @@ def gconnect():
     output += '<h1>Welcome, '
     output += login_session['username']
     output += '!</h1>'
-    print "done!"
     return output
 
 # User Helper Functions
